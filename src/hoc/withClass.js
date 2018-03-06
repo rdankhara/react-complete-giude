ebc3612f97693c4c216ecc25ca5b175133b51b40
Rajnikant Dankhara <rajnikant.dankhara@gmail.com>
2018-03-06 23:46:08 +0000

    Alternative Way for Wrapped function return anonymous class

--- src/hoc/withClass.js
+++ src/hoc/withClass.js
@@ -1,9 +1,23 @@
-import React from 'react';
+import React, {Component} from 'react';
+
+// const withClass = (WrappedComponent, classname) => {
+//     return (props) => (<div className={classname} >
+//     <WrappedComponent {...props}/>
+//     </div>);
+// }
+
+//or using statteful component as below
 
 const withClass = (WrappedComponent, classname) => {
-    return (props) => (<div className={classname} >
-    <WrappedComponent {...props}/>
-    </div>);
+    return class extends Component{
+        constructor(props){
+            super(props);
+        }
+        render(){
+            return (<div className={classname} >
+                <WrappedComponent {...this.props}/>
+                </div>);
+        }
+    }
 }
-
 export default withClass;
